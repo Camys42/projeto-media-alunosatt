f7f1b84c6fab8543f23f2e0569b8404f0e3fa614
Camys42 <camilly.morais4@etec.sp.gov.br>
2024-11-20 09:09:46 -0300

    Adicionada verificação de recuperação

--- media.py
+++ media.py
@@ -9,6 +9,11 @@ def verificar_aprovacao(media):
     else:
         return "Reprovado"
 
+def verificar_recuperacao(media):
+    if 5.0 <= media < 6.0:
+        return "Recuperação"
+    return ""
+
 # Recebe as notas do aluno
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
@@ -20,3 +25,4 @@ media = calcular_media(nota1, nota2, nota3)
 # Exibe o resultado
 print(f"A média do aluno é: {media:.2f}")
 print(verificar_aprovacao(media))  # Mensagem de aprovação ou reprovação
+print(verificar_recuperacao(media))  # Mensagem de recuperação
