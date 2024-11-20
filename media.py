af2893d9818d6f0bc7952ea4a3df86a2a93ed220
Camys42 <camilly.morais4@etec.sp.gov.br>
2024-11-20 09:08:51 -0300

    Adicionada verificação de aprovação

--- media.py
+++ media.py
@@ -1,8 +1,13 @@
 # media.py
 
-# Função para calcular a média simples das 3 notas
 def calcular_media(nota1, nota2, nota3):
     return (nota1 + nota2 + nota3) / 3
+
+def verificar_aprovacao(media):
+    if media >= 6:
+        return "Aprovado"
+    else:
+        return "Reprovado"
 
 # Recebe as notas do aluno
 nota1 = float(input("Digite a primeira nota: "))
@@ -14,3 +19,4 @@ media = calcular_media(nota1, nota2, nota3)
 
 # Exibe o resultado
 print(f"A média do aluno é: {media:.2f}")
+print(verificar_aprovacao(media))  # Mensagem de aprovação ou reprovação
